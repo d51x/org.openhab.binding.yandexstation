ccc48668c104277a25042686adfbb6b8c75b28e1
Dmitry P. (d51x) <dimonich110@gmail.com>
2023-12-26 13:13:50 +0300

    [yandexstation] issues/24: Exclude IPv6

--- src/main/java/org/openhab/binding/yandexstation/internal/YandexStationHandler.java
+++ src/main/java/org/openhab/binding/yandexstation/internal/YandexStationHandler.java
@@ -646,7 +646,8 @@ public class YandexStationHandler extends BaseThingHandler {
 
             // config.hostname = device.networkInfo.ipAdresses.get(0);
             config.hostname = Objects.requireNonNull(device.networkInfo.ipAdresses.stream()
-                    .filter(ip -> !ip.startsWith("169.254")).filter(ip -> !ip.contains(":")).findFirst().orElse(null));
+                    .filter(ip -> !ip.startsWith("169.254") && !ip.contains(":"))
+                    .findFirst().orElse(null));
             config.port = String.valueOf(device.networkInfo.port);
 
             Configuration configuration = thing.getConfiguration();
@@ -677,7 +678,8 @@ public class YandexStationHandler extends BaseThingHandler {
         properties.put("Wifi SSID:", device.networkInfo.wifiSSID);
         // properties.put("IP Address:", device.networkInfo.ipAdresses.get(0));
         properties.put("IP Address:", Objects.requireNonNull(device.networkInfo.ipAdresses.stream()
-                .filter(ip -> !ip.startsWith("169.254")).filter(ip -> !ip.contains(":")).findFirst().orElse(null)));
+                .filter(ip -> !ip.startsWith("169.254") && !ip.contains(":"))
+                .findFirst().orElse(null)));
         properties.put("Platform:", device.platform);
         properties.put("Device Name:", YandexStationTypes.getNameByPlatform(device.platform));
         properties.put("Friendly Name:", device.name);
